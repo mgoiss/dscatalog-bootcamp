fc5f39a493aafe028f599ae134ea364e6b4d8d74
Matheus Gois <mrochagois@gmail.com>
2020-10-02 13:29:04 -0300

    Database Seeding

--- backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dvsuperior.dscatalog.entities.Category;
 import com.dvsuperior.dscatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	private CategoryRepository reposytory;
 	
 	//Metodo responsável por pegar todoas as categorias no banco
+	@Transactional(readOnly = true) //Essa Another vai fazer com que a transação siga as PROPRIEDADE ASSINCRONA (onde ou faz tudo ou não faz nd)
 	public List<Category> findAll() {
 		return reposytory.findAll();
 	}
