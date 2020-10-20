eeac419865a6de5312812d59e1be61e822008efd
Matheus Gois <mrochagois@gmail.com>
2020-10-20 19:39:41 -0300

    Custom validation response

---
+++ backend/src/main/java/com/dvsuperior/dscatalog/resources/exceptions/FieldMessage.java
@@ -0,0 +1,35 @@
+package com.dvsuperior.dscatalog.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fielName;
+	private String message;
+	
+	public FieldMessage() {		
+	}
+
+	public FieldMessage(String fielName, String message) {
+		super();
+		this.fielName = fielName;
+		this.message = message;
+	}
+
+	public String getFielName() {
+		return fielName;
+	}
+
+	public void setFielName(String fielName) {
+		this.fielName = fielName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}
