7cc1c61870d93521d2aca3c928a21ee6e1441066
Matheus Gois <mrochagois@gmail.com>
2020-10-22 21:02:10 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/dvsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.dvsuperior.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 //Classe de configuração responsável por manter alguma configuração, por criar um componenete especifico...
 @Configuration
@@ -11,5 +13,17 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
