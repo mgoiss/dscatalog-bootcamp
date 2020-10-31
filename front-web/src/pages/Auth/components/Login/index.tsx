d7ac06adef4d49b2e16798d13c00cfde11de8ca7
Matheus Gois <mrochagois@gmail.com>
2020-10-31 20:33:09 -0300

    Make login and save user session data

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,39 +1,56 @@
 import ButtonIcon from 'core/components/Buttonicon';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
-
+        makeLogin(data)
+        .then(response => {
+            setHasError(false);
+            saveSessionData(response.data);
+            history.push('/admin')
+        })
+        .catch(() => {
+            setHasError(true);
+        });
     }
 
     return (
         <div>
             <AuthCard title="login">
+                {hasError && (
+                    <div className="alert alert-danger mt-5">
+                        Usuário ou senha inválidos!
+                    </div>
+                )}
                 <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                     <input
                         type="email"
                         className="form-control input-base margin-botton-30"
                         placeholder="Email"
-                        name="email"
-                        ref={register}
+                        name="username"
+                        ref={register({ required: true})}
                     />
                     <input
                         type="password"
                         className="form-control input-base"
                         placeholder="Senha"
                         name="password"
-                        ref={register}
+                        ref={register({ required: true})}
                     />
                     <Link to="/admin/auth/recover" className="login-link-recover">
                         Esqueci a senha?
@@ -43,10 +60,10 @@ const Login = () => {
                     </div>
                     <div className="text-center">
                         <span className="not-registered">
-                            Não tem cadastro?                            
+                            Não tem cadastro?
                         </span>
                         <Link to="/admin/auth/register" className="login-link-register">
-                                CADASTRAR
+                            CADASTRAR
                         </Link>
                     </div>
                 </form>
